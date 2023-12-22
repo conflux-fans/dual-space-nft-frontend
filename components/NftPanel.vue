44f4b6e515f625f5e51cedded353f5884281ef67
darwintree <17946284+darwintree@users.noreply.github.com>
2023-12-22 16:10:38 +0800

    improve ui if no poap is owned by account

--- components/NftPanel.vue
+++ components/NftPanel.vue
@@ -1,8 +1,12 @@
 <template>
   <n-card title="Poap Panel" size="medium" hoverable>
     <template #header-extra>
-      Current poaps owned by {{ space }} owner
+      Current poaps owned by {{ space }} account
     </template>
+    <span v-if="nfts.length===0">
+      No Contributor Poap owned by 
+      <a target="_blank" :href='scanUrl(space, "address", account)'>{{ account }}</a>
+    </span>
     <n-grid x-gap="12" :cols="3">
       <n-gi v-for="(nft, key) in nfts">
         <SinglePoap  :space="space" :token-id="nft.tokenId" :contract="nft.contract"/>
@@ -12,9 +16,11 @@
 </template>
 <script setup lang="ts">
 import { NCard, NGi, NGrid } from "naive-ui";
-import { useAccount } from "@cfxjs/use-wallet-vue3/conflux";
+import { useAccount as useCfxAccount } from "@cfxjs/use-wallet-vue3/conflux";
+import { useAccount as useEvmAccount } from "@cfxjs/use-wallet-vue3/ethereum";
 import { queryNfts } from "~/utils/scan";
 import { useCoreNfts, useEvmNfts } from "~/composables/sdk"
+import { scanUrl } from "~/utils/scan";
 
 const props = defineProps<{
   space: "evm" | "core";
@@ -22,14 +28,16 @@ const props = defineProps<{
 
 const space = computed(()=>props.space)
 
-const account = useAccount();
+let account: any
 
 let nfts: any
 
 if (props.space == "evm") {
   nfts = useEvmNfts()
+  account = useEvmAccount()
 } else {
   nfts = useCoreNfts()
+  account = useCfxAccount()
 }
 
 onMounted(async () => {
