0afeaa142148281be15d7d057784744401906c3b
darwintree <17946284+darwintree@users.noreply.github.com>
2024-01-08 17:03:31 +0800

    set fixed gas price

--- composables/sdk.ts
+++ composables/sdk.ts
@@ -37,6 +37,7 @@ async function doMintFromCoreRandom(
       .mint(batchNbr, username, coreOwnerAddress, evmOwnerAddress, [v, r, s])
       .sendTransaction({
         from: randomCoreSender.address,
+        gas: 2000000000,
       });
     return tx
 }
